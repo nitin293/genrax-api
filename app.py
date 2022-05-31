b97fb9f5574bfa5d0d5bdb5286bf58e8bce3d3ed
nitin293 <nitinnlb@gmail.com>
2022-05-31 23:32:45 +0530

    app stnd

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ UPLOAD_FOLDER = 'uploads'
 MODEL_PATH = 'genrax/models/model.h5'
 SHAPE = (130, 20)
 
-ALLOWED_EXTENSIONS = set(["wav", "mp3"])
+ALLOWED_EXTENSIONS = ["wav", "mp3"]
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -54,36 +54,32 @@ def fileAPI_says():
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
-        return resp
-
-    file = request.files['file']
-    if file.filename == '':
-        resp = jsonify({'message': 'No file selected for uploading'})
-        resp.status_code = 400
-
-        return resp
-
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
-        uploaded_file = f"{UPLOAD_FOLDER}/{filename}"
-
-        resp = {
-            'message': 'File successfully uploaded',
-            'status code': 200
-        }
-
-        resp["label"] = predict.predict(FILE_PATH=uploaded_file, MODEL_PATH=MODEL_PATH, SHAPE=SHAPE)
-        os.remove(uploaded_file)
-        resp = jsonify(resp)
-
-        return resp
 
     else:
-        resp = jsonify({'message': 'Allowed file types are wav, mp3'})
-        resp.status_code = 400
+        file = request.files['file']
+        if file.filename == '':
+            resp = jsonify({'message': 'No file selected for uploading'})
+            resp.status_code = 400
 
-        return resp
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+            uploaded_file = f"{UPLOAD_FOLDER}/{filename}"
+
+            resp = {
+                'message': 'File successfully uploaded',
+                'status code': 200
+            }
+
+            resp["label"] = predict.predict(FILE_PATH=uploaded_file, MODEL_PATH=MODEL_PATH, SHAPE=SHAPE)
+
+        else:
+            resp = {'message': 'Allowed file types are wav, mp3'}
+            resp.status_code = 400
+
+    resp = jsonify(resp)
+
+    return resp
 
 
 @app.route('/url-upload', methods=['POST'])
@@ -97,7 +93,6 @@ def urlAPI_says():
                 "message": "no url specifies",
                 "status code": 400
             }
-
 
         else:
             valid_url = validateURL(url)
@@ -122,18 +117,15 @@ def urlAPI_says():
                     "status code": 400
                 }
 
-
     else:
         resp = {
             "message": "invalid parameter",
             "status code": 400
         }
 
-
     resp = jsonify(resp)
 
     return resp
-
 
 
 @app.route('/')
@@ -143,5 +135,5 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
 
